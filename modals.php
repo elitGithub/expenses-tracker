12ede78f858a0c4f983949eda12a0c50c91274d6
KrenTrox <krentrox@gmail.com>
2024-04-19 19:27:30 +0300

    refactor: code in expense_report.php and user_management.php

--- modals.php
+++ modals.php
@@ -43,42 +43,41 @@ $expenseCategories = $expenseCategoryList->getAllCategories();
             <div class="panel-body">
                 <!-- Modal -->
                 <div class="modal fade" id="addExpenseModal" tabindex="-1" aria-labelledby="addExpenseModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
+                    <div class="modal-dialog modal-dialog-centered">
                         <div class="modal-content">
                             <div class="modal-header d-flex justify-content-between">
                                 <h4 class="modal-title" id="addExpenseModalLabel">
                                     <i class="fa fa-plus-circle fa-1x"></i> Add Expenses
                                 </h4>
-                                <!-- 'data-bs-dismiss' attribute for Bootstrap 5 -->
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 <form action="index.php?action=add_expense" method="POST" enctype="multipart/form-data">
                                     <div class="row">
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col-md-6 mb-3">
                                             <label for="add_expense_category_id">Expense Name:</label>
-                                            <select class="form-control" name="expense_category_id" id="add_expense_category_id" required>
+                                            <select class="form-control mt-2" name="expense_category_id" id="add_expense_category_id" required>
                                                 <option value="" selected>Choose Expense Category</option>
                                                 <?php
                                                 echo join('', $expenseCategoryList->getAllCategories(true)) ?>
                                             </select>
                                         </div>
 
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col-md-6 mb-3">
                                             <label for="add_expense_amount_spent">Amount Spent:</label>
-                                            <input type="number" class="form-control" name="amount_spent" id="add_expense_amount_spent" placeholder="Please Enter Expense Amount :" required>
+                                            <input type="number" class="form-control mt-2" name="amount_spent" id="add_expense_amount_spent" placeholder="Please Enter Expense Amount :" required>
                                         </div>
                                     </div>
 
                                     <div class="row">
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col-md-6 mb-3">
                                             <label for="add_expense_description">Expense Description:</label>
-                                            <input type="text" class="form-control" name="expense_description" id="add_expense_description" placeholder="Please Enter Expense Description :" required>
+                                            <input type="text" class="form-control mt-2" name="expense_description" id="add_expense_description" placeholder="Please Enter Expense Description :" required>
                                         </div>
 
-                                        <div class="form-group col-md-6">
+                                        <div class="form-group col-md-6 mb-3">
                                             <label for="add_expense_date">Date:</label>
-                                            <input type="date" class="form-control" name="expense_date" id="add_expense_date" required>
+                                            <input type="date" class="form-control mt-2" name="expense_date" id="add_expense_date" required>
                                         </div>
                                     </div>
 
@@ -101,44 +100,43 @@ $expenseCategories = $expenseCategoryList->getAllCategories();
     <?php if (PermissionsManager::isPermittedAction('edit_expense', $current_user)) : ?>
         <div class="panel-body">
             <!-- Modal -->
-            <div class="modal fade" id="editExpenseModal" aria-hidden="true" aria-labelledby="myModalLabel">
-                <div class="modal-dialog">
+            <div class="modal fade" id="editExpenseModal" aria-hidden="true" aria-labelledby="editExpenseModalLabel">
+                <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
-                        <div class="modal-header">
-                            <!-- 'data-bs-dismiss' attribute for Bootstrap 5 -->
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            <h4 class="modal-title" id="myModalLabel">
-                                <i class="fa fa-plus-circle fa-1x"></i> Edit Expense
+                        <div class="modal-header d-flex justify-content-between">
+                            <h4 class="modal-title" id="editExpenseModalLabel">
+                                <i class="fa fa-edit fa-1x"></i> Edit Expenses
                             </h4>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             <form action="index.php?action=update_expense" method="POST" enctype="multipart/form-data">
                                 <input type="hidden" name="expense_id">
                                 <div class="row">
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-6 mb-2">
                                         <label for="expense_category_id">Expense Category:</label>
-                                        <select class="form-control" name="expense_category_id" id="expense_category_id" required>
+                                        <select class="form-control mt-2" name="expense_category_id" id="expense_category_id" required>
                                             <option disabled value="" selected>Choose Expense Category</option>
                                             <?php
                                             echo join('', $expenseCategoryList->getAllCategories(true)) ?>
                                         </select>
                                     </div>
 
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-6 mb-2">
                                         <label for="amount_spent">Amount Spent:</label>
-                                        <input type="text" class="form-control" name="amount_spent" id="amount_spent" placeholder="Please Enter Expense Amount :" required>
+                                        <input type="text" class="form-control mt-2" name="amount_spent" id="amount_spent" placeholder="Please Enter Expense Amount :" required>
                                     </div>
                                 </div>
 
                                 <div class="row">
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-6 mb-2">
                                         <label for="expense_description">Expense Description:</label>
-                                        <input type="text" class="form-control" name="expense_description" id="expense_description" placeholder="Please Enter Expense Description :" required>
+                                        <input type="text" class="form-control mt-2" name="expense_description" id="expense_description" placeholder="Please Enter Expense Description :" required>
                                     </div>
 
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-6 mb-2">
                                         <label for="expense_date">Date:</label>
-                                        <input type="date" class="form-control" name="expense_date" id="expense_date" required>
+                                        <input type="date" class="form-control mt-2" name="expense_date" id="expense_date" required>
                                     </div>
                                 </div>
 
@@ -234,7 +232,7 @@ $expenseCategories = $expenseCategoryList->getAllCategories();
     endif; ?>
     <?php if (PermissionsManager::isPermittedAction('edit_expense_category', $current_user)) : ?>
         <div class="modal fade" id="editCategoryModal" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header d-flex justify-content-between">
                         <h4 class="modal-title" id="addCategoryModalLabel">
@@ -244,16 +242,16 @@ $expenseCategories = $expenseCategoryList->getAllCategories();
                     </div>
                     <div class="modal-body">
                         <form action="index.php?action=update_expense_category" method="POST">
-                            <div class="form-group">
+                            <div class="form-group mb-3">
                                 <label for="expense_category_name">Category Name:</label>
-                                <input type="text" class="form-control" name="expense_category_name" id="expense_category_name" required>
+                                <input type="text" class="form-control mt-2" name="expense_category_name" id="expense_category_name" required>
                             </div>
 
-                            <div class="form-group">
+                            <div class="form-group mb-3">
                                 <label for="expense_category_budget">Category Budget:</label>
-                                <input type="number" class="form-control" name="expense_category_budget" id="expense_category_budget" required>
+                                <input type="number" class="form-control mt-2" name="expense_category_budget" id="expense_category_budget" required>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group mb-3">
                                 <input type="checkbox" class="form-check-input" name="is_default" id="is_default">
                                 <label for="is_default">Set this category as default</label>
                             </div>
@@ -277,12 +275,14 @@ $expenseCategories = $expenseCategoryList->getAllCategories();
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h3 class="modal-title" id="deleteCategoryModalLabel">System</h3>
+                            <h4 class="modal-title" id="deleteCategoryModalLabel">
+                                <i class="fa fa-warning fa-1x"></i> System
+                            </h4>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <form method="POST" action="index.php?action=delete_category">
                             <div class="modal-body text-center">
-                                <h4>Are you sure you want to delete this category All expenses will be moved to the category marked as default.</h4>
+                                <p>Are you sure you want to delete this category All expenses will be moved to the category marked as default.</p>
                                 <!-- Hidden input for CSRF protection -->
                                 <input type="hidden" id="del_category_id" name="category_id">
                                 <input type="hidden" name="formToken" value="<?php
@@ -521,7 +521,7 @@ $expenseCategories = $expenseCategoryList->getAllCategories();
                                 <!-- Hidden input for CSRF protection -->
                                 <input type="hidden" id="del_user_id" name="userId">
                                 <input type="hidden" name="formToken" value="<?php
-                                echo htmlspecialchars($deleteUserToken); ?>">
+                                                                                echo htmlspecialchars($deleteUserToken); ?>">
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
